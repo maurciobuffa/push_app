522bc28d0e0923ccf43c17065064927e67373f2c
Mauricio Buffa <mauriciobuffa@MacBook-Pro-de-Mauricio.local>
2023-06-13 19:30:41 -0300

    first commit

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: context
+            .select((NotificationsBloc bloc) => Text('${bloc.state.status}')),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<NotificationsBloc>().requestPermission();
+            },
+            icon: const Icon(Icons.settings),
+          )
+        ],
+      ),
+      body: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
